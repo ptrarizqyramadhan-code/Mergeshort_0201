eb9a00abd86451bb1903635f0815fe25829a1a99
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-06-09 21:56:44 +0700

    step10

--- MergeShort.cpp
+++ MergeShort.cpp
@@ -75,3 +75,34 @@ void mergeSort(int low, int high)
         j++;
         k++;
     }
+
+    while (i <= mid) // step 4.f
+    {
+        B[k] = arr[i]; // step 4.f.i
+        i++;            // step 4.f.ii
+        k++;            // step 4.f.iii
+    }
+    
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr [x] = B [x];
+    }
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n-1);
+    output();
+}
